88382c988c66471a8e2a240ac1794b9c64a01b79
Raj Shah <shahrajyogesh@gmail.com>
2023-04-02 21:38:33 +0530

    First Commit

---
+++ src/main/java/com/bookclub/BookClubApplication.java
@@ -0,0 +1,16 @@
+package com.bookclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookClubApplication.class, args);
+
+		System.out.println("Hi, Let's goooooo!!");
+
+	}
+
+}
